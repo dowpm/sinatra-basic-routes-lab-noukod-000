dcceaa20715a4c63c9a0595e9c3453f5a6c290be
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-05 19:56:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    'Hello, World!'
+  end 
+  
+  get '/name' do
+    'My name is Mac Dowell'
+  end 
+  
+  get '/hometown' do 
+    'My hometown is 20, rue père primet, Saint-Marc, Haiti'
+  end
+  
+  get '/favorite-song' do
+    'My favorite song is Tanperaman of King of Kings'
+  end
 end
